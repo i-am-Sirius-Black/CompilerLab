d8b9614042cd528a3f8e8cbf0738913cbe64b0ce
i-am-Sirius-Black <khanjaved.sk19@gmail.com>
2023-09-13 18:10:58 +0530

    basic compiler for c

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const {generateFile} =require ('./generateFile');
 const { executeCpp } = require('./executeCpp');
+const cors = require('cors');
 
 //middlewares
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+const [code, setCode] = useState('');
+const [output, setOutput] = useState('');
+
+const handleSubmit = async () => {
+  const payload = {
+    language: 'cpp',
+    code
+  }
+
+  try {
+    const {data} = await axios.post('http://localhost:5000/run', payload);
+    setOutput(data.output);
+  } catch (error) {
+     console.log(error.response);
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>CompilerLab</h1>
+      <p>Online Compiler</p>
+      {/* <select>
+        <option value='java'>Java</option>
+        <option value='cpp'>C++</option>
+        <option value='c'>C</option>
+        <option value='py'>Python</option>
+      </select> */}
+       <textarea rows='20' cols='80' 
+        value={code}
+        onChange={(e)=>{
+        setCode(e.target.value);
+        }} > </textarea>
+
+      <button className='submit-button' onClick={handleSubmit}>
+        Submit
+        </button>
+        <p>{output}</p>
     </div>
   );
 }
